e45049fb0ef01b0837afaad07a892da7692573fc
LonelyRookie <Abel_hcc@outlook.com>
2018-08-10 13:11:21 +0800

    添加了toString()方法

--- vmt/src/com/monitoring/bean/UserBean.java
+++ vmt/src/com/monitoring/bean/UserBean.java
@@ -8,95 +8,104 @@ package com.monitoring.bean;
  * @date 2018/03/08
  */
 public class UserBean {
-    private String roleName = "";
-    private String regionName = "";
-    private String countryName = "";
-    private String userStatus = "";
-    private String userISID = "";
+	private String roleName = "";
+	private String regionName = "";
+	private String countryName = "";
+	private String userStatus = "";
+	private String userISID = "";
 
-    private String roleEnrolled = "";
-    private String countriesEnrolled = "";
-    private String regionEnrolled = "";
-    private String userEnrolled = "";
-    private String statusEnrolled = "";
+	private String roleEnrolled = "";
+	private String countriesEnrolled = "";
+	private String regionEnrolled = "";
+	private String userEnrolled = "";
+	private String statusEnrolled = "";
 
-    public String getRoleName() {
-        return roleName;
-    }
+	public String getRoleName() {
+		return roleName;
+	}
 
-    public void setRoleName(String roleName) {
-        this.roleName = roleName;
-    }
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
 
-    public String getRegionName() {
-        return regionName;
-    }
+	public String getRegionName() {
+		return regionName;
+	}
 
-    public void setRegionName(String regionName) {
-        this.regionName = regionName;
-    }
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
 
-    public String getCountryName() {
-        return countryName;
-    }
+	public String getCountryName() {
+		return countryName;
+	}
 
-    public void setCountryName(String countryName) {
-        this.countryName = countryName;
-    }
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
 
-    public String getUserStatus() {
-        return userStatus;
-    }
+	public String getUserStatus() {
+		return userStatus;
+	}
 
-    public void setUserStatus(String userStatus) {
-        this.userStatus = userStatus;
-    }
+	public void setUserStatus(String userStatus) {
+		this.userStatus = userStatus;
+	}
 
-    public String getUserISID() {
-        return userISID;
-    }
+	public String getUserISID() {
+		return userISID;
+	}
 
-    public void setUserISID(String userISID) {
-        this.userISID = userISID;
-    }
+	public void setUserISID(String userISID) {
+		this.userISID = userISID;
+	}
 
-    public String getRoleEnrolled() {
-        return roleEnrolled;
-    }
+	public String getRoleEnrolled() {
+		return roleEnrolled;
+	}
 
-    public void setRoleEnrolled(String roleEnrolled) {
-        this.roleEnrolled = roleEnrolled;
-    }
+	public void setRoleEnrolled(String roleEnrolled) {
+		this.roleEnrolled = roleEnrolled;
+	}
 
-    public String getCountriesEnrolled() {
-        return countriesEnrolled;
-    }
+	public String getCountriesEnrolled() {
+		return countriesEnrolled;
+	}
 
-    public void setCountriesEnrolled(String countriesEnrolled) {
-        this.countriesEnrolled = countriesEnrolled;
-    }
+	public void setCountriesEnrolled(String countriesEnrolled) {
+		this.countriesEnrolled = countriesEnrolled;
+	}
 
-    public String getRegionEnrolled() {
-        return regionEnrolled;
-    }
+	public String getRegionEnrolled() {
+		return regionEnrolled;
+	}
 
-    public void setRegionEnrolled(String regionEnrolled) {
-        this.regionEnrolled = regionEnrolled;
-    }
+	public void setRegionEnrolled(String regionEnrolled) {
+		this.regionEnrolled = regionEnrolled;
+	}
 
-    public String getUserEnrolled() {
-        return userEnrolled;
-    }
+	public String getUserEnrolled() {
+		return userEnrolled;
+	}
 
-    public void setUserEnrolled(String userEnrolled) {
-        this.userEnrolled = userEnrolled;
-    }
+	public void setUserEnrolled(String userEnrolled) {
+		this.userEnrolled = userEnrolled;
+	}
 
-    public String getStatusEnrolled() {
-        return statusEnrolled;
-    }
+	public String getStatusEnrolled() {
+		return statusEnrolled;
+	}
 
-    public void setStatusEnrolled(String statusEnrolled) {
-        this.statusEnrolled = statusEnrolled;
-    }
+	public void setStatusEnrolled(String statusEnrolled) {
+		this.statusEnrolled = statusEnrolled;
+	}
+
+	@Override
+	public String toString() {
+		return "UserBean [roleName=" + roleName + ", regionName=" + regionName + ", countryName=" + countryName
+				+ ", userStatus=" + userStatus + ", userISID=" + userISID + ", roleEnrolled=" + roleEnrolled
+				+ ", countriesEnrolled=" + countriesEnrolled + ", regionEnrolled=" + regionEnrolled + ", userEnrolled="
+				+ userEnrolled + ", statusEnrolled=" + statusEnrolled + "]";
+	}
+
 }
